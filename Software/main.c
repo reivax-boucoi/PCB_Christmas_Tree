841b7c08cca706f620b340f0ff0f82f7faeb69f2
reivax-boucoi <reivax.boucoi@laposte.net>
2021-07-11 15:54:46 +0200

    a

--- Software/main.c
+++ Software/main.c
@@ -21,7 +21,8 @@ volatile uint8_t level[12];
 //Select order based on led soldering polarity
 const uint8_t order[12] = {0,11,9,4,2,10,7,6,3,1,8,5};//Reverse mounted leds ordering
 //const uint8_t order[12] = {3,8,2,7,9,4,5,1,0,6,11,10};//Right orientation for leds
-volatile uint8_t blinkMode = SCROLL;
+
+volatile uint8_t blinkMode = SCROLL;    //Default animation at powerup
 volatile uint8_t pstate=0;
 /*
 static	long	randx;
