93d5f91fff981fc290603e5bc47fde937788f00d
reivax-boucoi <reivax.boucoi@laposte.net>
2021-07-13 15:54:00 +0200

    a

--- Software/main.c
+++ Software/main.c
@@ -17,8 +17,10 @@
 
 volatile uint8_t cnt=1;
 volatile uint8_t level[12];
+
+//Select
 const uint8_t order[12] = {0,11,9,4,2,10,7,6,3,1,8,5};//Reverse mounted leds ordering
-//const uint8_t order[12] = {3,8,2,7,9,4,5,1,0,6,11,10};//Right
+//const uint8_t order[12] = {3,8,2,7,9,4,5,1,0,6,11,10};//Right orientation for leds
 volatile uint8_t blinkMode = SCROLL;
 volatile uint8_t pstate=0;
 /*
