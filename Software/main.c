83efbf11302a2f5f68e272194e508a1abe3af4d5
reivax-boucoi <reivax.boucoi@laposte.net>
2021-11-20 21:11:16 +0100

    added code

--- Software/main.c
+++ Software/main.c
@@ -7,10 +7,14 @@
 #define PIXIE 1
 #define DEBUG 2
 
+#define TOUCH_FLAG 0
+#define ADC_FLAG 1
+#define DEBUG_FLAG 2
+
 volatile unsigned int cnt=0;
 uint8_t level[12];
 uint8_t level[12] = {1, 1, 3, 7, 15, 31, 63, 31, 15, 7, 3, 1 }; //Brightness levels of each led.
-uint8_t order[12] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};     //Map from left to right from top (0) to bottom (11)
+uint8_t order[12] = {6,1,9,2,3,0,4,7,8,11,10,5};//5, 1, 3, 4, 6, 11, 0, 7, 8, 2, 10, 9};     //Map from left to right from top (0) to bottom (11)
 uint8_t blinkMode = FADE;
 uint8_t pstate=0;
 int main(void){
@@ -27,49 +31,46 @@ int main(void){
     TCCR0B=(1<<CS02)|(1<<CS00);
     TIMSK|=(1<<OCIE0A);
     
+    
+    ADMUX=(1<<MUX3)|(1<<MUX2);
+    ADCSRA |= (1<<ADEN) | (1<<ADIE);
+    
     sei();                              //Enable interrupts
     while(1){
         
-        
-       /*for (uint8_t i=0;i<12;i++){
-        level[i]=63;
+        if((PINB&(1<<PINB4)) && (pstate&(1<<TOUCH_FLAG))){
+            pstate&=~(1<<TOUCH_FLAG);
+            blinkMode++;
+            if(blinkMode>11)blinkMode=0;
+        }else if(!(PINB&(1<<PINB4)) && !(pstate&(1<<TOUCH_FLAG))){
+            pstate|=(1<<TOUCH_FLAG);            
         }
-        _delay_ms(1500);
-        */
-       
-        for (uint8_t i=0;i<12;i++){
-            level[i]=0;
+
+        if(pstate&(1<<ADC_FLAG)){
+            pstate&=~(1<<ADC_FLAG);   
+            ADCSRA |= (1<<ADSC);
         }
-        level[blinkMode]=(cnt>20)?63:0;
-        _delay_ms(10);
-        
-        uint8_t state=PINB&(1<<PINB4);
-        if(state!= pstate){
-            pstate=state;
-            if(state){
-                blinkMode++;
-                if(blinkMode>2)blinkMode=0;
-            }
-        }
-        
-        /*
-         uint8_t temp = level[11];
-        for (int i=11; i>0; i--) level[i] = level[i-1];
-        level[0] = temp;
-         _delay_ms(100);*/
     }
     return 0;
 }     
 
+ISR(ADC_vect){
+    uint16_t res=ADCL;
+    res|=(ADCH<<8);
+    blinkMode=2753/res-3;   //Vcc=1024*1.1/ADC
+}
 
 ISR(TIM0_COMPA_vect) {
     cnt++;
-    if(cnt>21){
+    if(cnt>11){
         cnt=0;
-        blinkMode++;
-        if(blinkMode>11)blinkMode=0;
+        pstate^=(1<<DEBUG_FLAG);
+        if(pstate&(1<<DEBUG_FLAG))pstate|=(1<<ADC_FLAG);   
     }
+    level[order[cnt]]=(cnt<=blinkMode)?63:0;
+    
 }
+
 
 ISR(TIM1_COMPA_vect) {
   static uint8_t first, ramp, column, bits, colbit;
